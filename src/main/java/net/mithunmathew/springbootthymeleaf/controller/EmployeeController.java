1f08c170a6380d175e8f7a44f4de799105c98b0b
Mithun Mathew <mithunmathew007@gmail.com>
2021-05-15 01:46:15 +0530

    add employee functionality added

--- src/main/java/net/mithunmathew/springbootthymeleaf/controller/EmployeeController.java
+++ src/main/java/net/mithunmathew/springbootthymeleaf/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.mithunmathew.springbootthymeleaf.entity.Employee;
@@ -31,6 +33,27 @@ public class EmployeeController {
 		// add employees to the Spring MVC model
 		theModel.addAttribute("employees", employees);
 		
-		return "employee-list";
+		return "employees/employee-list";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";		
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save to db
+		employeeService.save(theEmployee);
+		
+		// redirect to listing page
+		return "redirect:/employees/list";
 	}
 }
--- src/main/java/net/mithunmathew/springbootthymeleaf/dao/EmployeeRepository.java
+++ src/main/java/net/mithunmathew/springbootthymeleaf/dao/EmployeeRepository.java
@@ -1,9 +1,16 @@
 package net.mithunmathew.springbootthymeleaf.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.mithunmathew.springbootthymeleaf.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// auto generate code to operate on DB
+	
+	// sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
 
 }
